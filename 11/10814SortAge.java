742037bbd21cc5f4cf7112a8f107e0739c13ca3e
신윤정 <kimmy100b@gmail.com>
2022-07-12 11:46:57 +0900

    [11단계] 10814 나이순 정렬

--- 11/10814SortAge.java
+++ 11/10814SortAge.java
@@ -1,6 +1,6 @@
 /**
  *  번호: 10814
- *  날짜: 2022-06-08
+ *  날짜: 2022-07-12
  *  제목: 나이순 정렬
  *  링크: https://www.acmicpc.net/problem/10814
  * 
@@ -8,29 +8,55 @@
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.*;
+ 
 
-class SortAge {
+class SortAge { // SortAge
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        String[][] usrArr = new String[N][2];
+
+        // 클래스 배열 생성
+		User[] userList = new User[N];
 
         for(int i = 0; i < N; i++){
             int age = sc.nextInt();
-            usrArr[i][0] = String.valueOf(age);
-            usrArr[i][1] = sc.nextLine();
+            String name = sc.nextLine();
+            userList[i] = new User(age, name);
         }
 
-        for(int i = 0; i < N; i++){
-            String[] temp = new String[N];
-            for(int j = i + 1; j < N; j++) {
-                if(Integer.valueOf(usrArr[i][0]) > Integer.valueOf(usrArr[j][0])) {
-                    temp = usrArr[i];
-                    usrArr[i] = usrArr[j];
-                    usrArr[j] = temp;
-                }
-            }
-            System.out.println(usrArr[i][0] + " " + usrArr[i][1]);
+        Arrays.sort(userList); // compareTo 함수가 없으면 ERROR
+        
+        for (User usr : userList) {
+            System.out.println(usr.toString());
         }
+    }
+}
+
+class User implements Comparable<User> {
+    private int age;
+    private String name;
+
+    User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public int compareTo(User usr)
+    {
+        if (this.age != usr.getAge()) {
+            return this.age - usr.getAge();
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return this.age + this.name;
     }
 }
